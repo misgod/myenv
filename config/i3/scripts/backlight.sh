e1b45f07caf9416ed5b7e208bad3100241e17184
misgod <misgod.tw@gmail.com>
2015-11-12 00:13:45 +0800

    add backlight script

---
+++ config/i3/scripts/backlight.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+show() {
+    xbacklight -get | { read i; printf "%.0f" $i; }
+}
+
+
+case $BLOCK_BUTTON in
+    3) xset dpms force off ;; # right click, turn off screen
+    4) xbacklight -inc 3;; #scroll up
+    5) xbacklight -dec 2;; #scroll down
+    *) ;;
+esac
+
+show
+
+   
