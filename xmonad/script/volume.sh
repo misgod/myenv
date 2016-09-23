2a03797ff9356e1b4a8bbb51f377e409088007d6
misgod <misgod.tw@gmail.com>
2016-09-23 11:23:55 +0800

    add volumn in xmobar

--- xmonad/script/volume.sh
+++ xmonad/script/volume.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 case $1 in
-     4) /usr/bin/amixer -q sset Master 5%+;;
-     5) /usr/bin/amixer -q sset Master 5%-;;
+     4) /usr/bin/amixer -q sset Master 3%+;;
+     5) /usr/bin/amixer -q sset Master 3%-;;
      3) /usr/bin/amixer -q sset Master toggle;;
      *) ;;
 esac
@@ -12,12 +12,12 @@ CURVOL=`amixer get Master |grep % |awk '{print $5}'|sed 's/[^0-9\%]//g' | head -
 #notify-send "Volume: $CURVOL"
 
 show() {
-	template="%.0f"
+	template="%s"
     for i in 3 4 5; do
        template="<action=\`$0 $i\` button=$i>$template</action>"
     done
 
-    CURVOL=`amixer get Master |grep % |awk '{print $5}'|sed 's/[^0-9]//g' | head -n1`
+    CURVOL=`amixer get Master |grep % |awk '{print $6=="[off]"?$6:$5}' | sed 's/[^0-9ONFonf]//g' | head -n1`
     printf "$template" $CURVOL
     
    # xbacklight -get | { read i; printf "$template" $i; }
