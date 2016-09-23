c43f49e40ec5bd1138b88b9aaa13283cebf5630a
misgod <misgod.tw@gmail.com>
2016-09-23 10:55:32 +0800

    add volume script

--- xmonad/script/volume.sh
+++ xmonad/script/volume.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 case $1 in
-     4) /usr/bin/amixer -q -c 0 sset 'Master' 5%+;;
-     5) /usr/bin/amixer -q -c 0 sset 'Master' 5%-;;
-     3) /usr/bin/amixer -q -c 0 sset 'Master' toggle;;
+     4) /usr/bin/amixer -q sset Master 5%+;;
+     5) /usr/bin/amixer -q sset Master 5%-;;
+     3) /usr/bin/amixer -q sset Master toggle;;
      *) ;;
 esac
 
-CURVOL=`amixer get Master | awk '/Mono.+/ {print $6=="[off]"?$6:$4}' | tr -d '[]'`
+CURVOL=`amixer get Master |grep % |awk '{print $5}'|sed 's/[^0-9\%]//g' | head -n1`
 
-notify-send "Volume: $CURVOL"
-
-
+#notify-send "Volume: $CURVOL"
 
 show() {
 	template="%.0f"
@@ -19,16 +17,11 @@ show() {
        template="<action=\`$0 $i\` button=$i>$template</action>"
     done
 
-    xbacklight -get | { read i; printf "$template" $i; }
+    CURVOL=`amixer get Master |grep % |awk '{print $5}'|sed 's/[^0-9]//g' | head -n1`
+    printf "$template" $CURVOL
+    
+   # xbacklight -get | { read i; printf "$template" $i; }
 }
-
-
-case $1 in
-    3) sleep 0.5;xset dpms force off ;; # right click, turn off screen
-    4) xbacklight -inc 3;; #scroll up
-    5) xbacklight -dec 2;; #scroll down
-    *) ;;
-esac
 
 show
 
